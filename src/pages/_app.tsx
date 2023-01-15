4469e995d81b993ee85a44a23f7a36a86eac83bc
robertkyang <robert.yang2003@gmail.com>
2023-01-14 22:56:56 -0500

    tried to do fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,21 @@ import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
+}
 
 export default api.withTRPC(MyApp);
